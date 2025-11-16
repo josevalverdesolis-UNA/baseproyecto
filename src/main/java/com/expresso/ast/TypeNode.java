8eeb6a8ed1c42495c82486f61abe8864e7b25470
josevalverdesolis-UNA <jose.valverde.solis@est.una.ac.cr>
2025-11-16 03:26:16 -0600

    quitar

--- src/main/java/com/expresso/ast/TypeNode.java
+++
@@ -1,9 +0,0 @@
-// Project: Expresso
-// Subject: EIF400 - Programming Paradigms - II-2025
-// Group: 03 6PM
-// Authors: Rodrigo C. , Eduardo F. , Diego S., Jose Luis V.
-
-package com.expresso.ast;
-
-public sealed interface TypeNode extends Node permits AtomicNode, TupleNode, ArrowNode, TypeVar {
-}
